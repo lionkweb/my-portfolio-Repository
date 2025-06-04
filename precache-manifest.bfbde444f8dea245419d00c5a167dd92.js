191f1496ed1b3053dfaa2f8cab0d4770aadc04c9
lionkweb <lionkweb@gmail.com>
2025-06-04 07:31:01 +0300

    Updates

---
+++ precache-manifest.bfbde444f8dea245419d00c5a167dd92.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0c98c885c979ac5f9273299dc55c5a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1691da6dfd271ba4c70",
+    "url": "/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "76abba105f326dd2eeb0",
+    "url": "/static/css/4.d7ceb645.chunk.css"
+  },
+  {
+    "revision": "961469e775dcd7e56352",
+    "url": "/static/css/main.bcb617d0.chunk.css"
+  },
+  {
+    "revision": "227b43719ee087cde66c",
+    "url": "/static/js/2.33fdafa8.chunk.js"
+  },
+  {
+    "revision": "a6c0177ea3999624a9f2d92abf620885",
+    "url": "/static/js/2.33fdafa8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1691da6dfd271ba4c70",
+    "url": "/static/js/3.cd13276d.chunk.js"
+  },
+  {
+    "revision": "76abba105f326dd2eeb0",
+    "url": "/static/js/4.b468ed4a.chunk.js"
+  },
+  {
+    "revision": "961469e775dcd7e56352",
+    "url": "/static/js/main.34f4152c.chunk.js"
+  },
+  {
+    "revision": "eb37b5850bfbe1b171d2",
+    "url": "/static/js/runtime-main.72dceb3e.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "e8f8a7501627b0fb3aa2fb732090ba42",
+    "url": "/static/media/BilgeAdam-Technologies-Logo.e8f8a750.png"
+  },
+  {
+    "revision": "ec0129194609351a82b383aaf889ac90",
+    "url": "/static/media/Masu-logo.ec012919.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "1ef2a4cdb29db201594201fa6e6bed6a",
+    "url": "/static/media/cankaya-universitesi-seeklogo.1ef2a4cd.png"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "bbad43dabfe344dcae6ef9b92c66f2a7",
+    "url": "/static/media/iTunes-logo.bbad43da.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "e1ca81096816328d4b3bd6b3b05c054f",
+    "url": "/static/media/petfinder.e1ca8109.png"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "4d78b2bf1fdd5feed7687b9877a215e0",
+    "url": "/static/media/resume.4d78b2bf.pdf"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  }
+]);
